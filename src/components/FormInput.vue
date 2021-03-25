498c25aba378e21feeb50d3a3a1e5b64ff863db4
emuzabies <emuzabies@gmail.com>
2021-03-25 18:26:59 +0700

    emu479p1-push

---
+++ src/components/FormInput.vue
@@ -0,0 +1,90 @@
+<template>
+<div>
+<div id="guyscard">
+<h5>ต้า Database :</h5>
+<a>Form จะใหญ่ไปไหนเนี่ย</a>
+<h5>บอมบ์ Full-stack Dev :</h5>
+<a>เออว่ะ แต่ขี้เกียจ Deployเว็บใหม่ว่ะ</a>
+<h5>ต้า Database :</h5>
+<a>แต่เดี๋ยวนะ ตอนที่มึงกำลังใส่โค้ดตอนนี้มันยังไม่Deploy และนี่ขนาดใส่ช่องchatสี ๆ เหมือนหน้าแรกมึงยังไม่ใส่เลย ปล่อยว่างโล้น ๆ</a>
+</div>
+<div class="card" id="guyscard">
+  <div class="card-body">
+<form @submit.prevent="handleSubmitForm">
+    <h1>กรอกข้อมูลให้ครบถ้วน แล้วกด Submit</h1>
+  <div class="form-group">
+    <label for="id">StudentID</label>
+    <input type="text" class="form-control"  v-model="user.userid" required>
+  </div>
+  <div class="form-group">
+    <label for="FirstName">Firstname</label>
+    <input type="text" class="form-control"  v-model="user.firstname" required>
+  </div>
+  <div class="form-group">
+    <label for="LastName">Lastname</label>
+    <input type="text" class="form-control"  v-model="user.lastname" required>
+  </div>
+    <div class="form-group">
+    <label for="BirthDate">Birthdate</label>
+    <input type="date" class="form-control" v-model="user.birthdate" required>
+  </div>
+  <div class="form-group">
+    <label for="Tel">Tel</label>
+    <input type="text" class="form-control"  v-model="user.tel" required>
+  </div>
+  <div class="form-group">
+    <label for="Email">E-mail</label>
+    <input type="email" class="form-control"  v-model="user.email" required>
+    <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+  </div>
+  <div class="form-group">
+  <button class="btn btn-primary">Submit</button>
+  </div>
+</form>
+  </div>
+</div>
+</div>
+</template>
+<script>
+import axios from 'axios'
+export default {
+    data(){
+        return{
+            user:{
+                userid: '',
+                firstname: '',
+                lastname: '',
+                tel: '',
+                email: '',
+                birthdate: ''
+            }
+        }
+    },
+    methods:{
+        handleSubmitForm(){
+          let apiURL = 'https://guys.azurewebsites.net/api/userinfo';
+
+          axios.post(apiURL, this.user).then(() => {
+            this.$router.push('/');
+            this.$user = {
+                userid: '',
+                firstname: '',
+                lastname: '',
+                tel: '',
+                email: '',
+                birthdate: ''
+            }
+          }).catch(error => {
+            console.log(error)
+          })
+
+        }
+    }
+    
+}
+</script>
+<style scoped>
+#guyscard{
+  margin: 5px 100px 5px 100px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<template>
+<div class="guys">
+  <div class="mb-2">
+    <b-card border-variant="secondary" text-variant="dark" header="ต้า Database" class="text-center">
+        <b-card-text>ทำได้แค่นี้หรอวะ เว็บเก็บข้อมูลมึงอ่ะ</b-card-text>
+      </b-card>
+  </div>
+  <div class="mb-2">
+      <b-card border-variant="info" text-variant="dark" header="บอม Full-stack Dev" class="text-center">
+        <b-card-text>เออ อยากสวยมึงก็ทำสิ</b-card-text>
+      </b-card>
+  </div>
+  <div class="mb-3">
+      <b-card border-variant="secondary" text-variant="dark" header="ต้า Database" class="text-center ">
+        <b-card-text>เหอะ ๆ ไม่อ่ะขี้เกียจ มึงทำอ่ะดีและ</b-card-text>
+      </b-card>
+  </div>
+      <b-card bg-variant="dark" text-variant="white" title="แย่แล้ว" header-tag="header" footer-tag="footer">
+      <template #header>
+        <h4 class="mb-0">ต้องการความช่วยเหลือ</h4>
+      </template>
+      <b-card-text >พวกเราทีมพัฒนา GUYs monitoring ขี้เกียจกรอกข้อมูลลงฐานข้อมูล ขอให้เพื่อน ๆ ช่วยกันกรอกข้อมูลหน่อยครับ <br> - GUYs Devepoler Team -</b-card-text>
+      <b-button to="/form" variant="primary">พร้อมแล้วลุยเลย</b-button>
+      <template #footer>
+        <em>*นี่ไม่ใช้โฆษณาเว็บพนันหรืออะไรแต่ต้องการข้อมูลจริง ๆ นะเอาไปโปรเจ็คง่ะ* <br>แต่ถ้าต้องการลงโฆษณาติดต่อ 094-154-1494  คุณกาย CEO</em>
+      </template>
+    </b-card>
+    <div class="mt-3">
+      <b-card border-variant="secondary" text-variant="dark" header="พื้นที่ใส่โฆษณา" class="text-center ">
+        <b-card-text>ตรงที่ว่างขาว ๆ ใส่โฆษณาได้นะงับ ราคาไม่แพง ยิงhostแล้ว มาหารายได้กัน</b-card-text>
+      </b-card>
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.guys{
+  text-align: center;
+}
+
+</style>
